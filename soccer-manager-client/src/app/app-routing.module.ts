f26db2404547e75634e2bf4336076ae23e930298
Michael Prattinger <mprattinger@outlook.com>
2018-03-19 16:05:36 +0100

    ng wip

--- soccer-manager-client/src/app/app-routing.module.ts
+++ soccer-manager-client/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
+import { AuthGuard } from './auth-guard.service';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { PathNotFoundComponent } from './path-not-found/path-not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'home' },
+  { path: 'home', component: HomeComponent, canActivate: [AuthGuard] },
+  { path: '**', pathMatch: 'full', component: PathNotFoundComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
