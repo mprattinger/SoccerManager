a139a169e96cbe52ab72a30167e06c80697fde7f
Michael Prattinger <mprattinger@outlook.com>
2018-03-21 15:41:04 +0100

    wip

---
+++ soccer-manager-client/src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { AuthModule } from './auth/auth.module';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+
+import { AppComponent } from './app.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ soccer-manager-client/src/app/auth/auth-routing.module.ts
@@ -0,0 +1,13 @@
+import { LoginComponent } from './login/login.component';
+import { NgModule, Component } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+const routes: Routes = [
+  { path: 'login', component: LoginComponent }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class AuthRoutingModule { }
